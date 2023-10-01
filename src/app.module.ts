5299091e603addec83344c58d8e4f556b0111750
kamio90 <kmusial40@gmail.com>
2023-10-01 04:24:11 +0200

    feat: add env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,9 @@ import { NewsFeedModule } from './news-feed/news-feed.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost/nest',
+    ),
     UserModule,
     CompanyModule,
     CityModule,
